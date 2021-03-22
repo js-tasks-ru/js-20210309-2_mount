09d679efc566290bb9a34b32350a1b0e3958f8d3
Mountcode <rolopoko@gmail.com>
2021-03-22 11:17:52 +0300

    task 1 complite

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,16 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
-
+  let [...destrPath] = path.split('.');
+  return function(targetObject){
+    if(targetObject){
+      for(var i = 0; i < destrPath.length; i++) {
+        if(typeof targetObject != "undefined"){
+          targetObject = targetObject[destrPath[i]];
+        }
+      }
+      return targetObject
+    }
+  }
 }
+
