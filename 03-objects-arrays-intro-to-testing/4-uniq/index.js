74704656945420a25fe0f98bf6314286b9c78c24
Mountcode <rolopoko@gmail.com>
2021-03-22 11:30:03 +0300

    task 4 complite

--- 03-objects-arrays-intro-to-testing/4-uniq/index.js
+++ 03-objects-arrays-intro-to-testing/4-uniq/index.js
@@ -3,6 +3,12 @@
  * @param {*[]} arr - the array of primitive values
  * @returns {*[]} - the new array with uniq values
  */
-export function uniq(arr) {
-
+export function uniq(arr = []) {
+  let uniqList = new Set();
+  let unicArr = [];
+  arr.forEach(function(item, i, arr) {
+    uniqList.add(item)
+  });
+  for (let value of uniqList) unicArr.push(value);
+  return unicArr;
 }
