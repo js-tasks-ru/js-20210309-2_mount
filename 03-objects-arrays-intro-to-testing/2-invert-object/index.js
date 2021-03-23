fa830fa11e35e791738cbf80fd96943ce61886ee
Mountcode <rolopoko@gmail.com>
2021-03-23 11:04:58 +0300

    task 2 complite

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,5 +4,14 @@
  * @returns {object | undefined} - returns new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
-
+  const mirrorObj = {}
+  if(typeof obj != "undefined"){
+    for(const [index, value] of Object.entries(obj)){
+      mirrorObj[value] = index;
+    }
+    return mirrorObj;
+  }
+  else{
+    return undefined;
+  }
 }
