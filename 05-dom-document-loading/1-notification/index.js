88c23fade1bbba3b4b688536ce22860886e659cd
Mountcode <rolopoko@gmail.com>
2021-03-29 12:00:00 +0300

    stage 5 task 1 complite

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,48 @@
 export default class NotificationMessage {
 
+  constructor(message,{
+    type = '',
+    duration = 0,
+  } = {}) {
+    this.message = message;
+    this.type = type;
+    this.duration = duration; 
+    this.render();
+  }
+
+  static activePopup;
+
+  show(popupContainer = document.body){
+    popupContainer.append(this.element);
+    setTimeout( ()=> this.remove(),this.duration);
+  }
+
+  render() {
+    if (NotificationMessage.activePopup) {
+      NotificationMessage.activePopup.remove(); 
+    }
+    const element = document.createElement('div');
+    element.innerHTML = `
+    <div class="notification ${this.type}" style="--value:${this.duration / 1000}s">
+      <div class="timer"></div>
+      <div class="inner-wrapper">
+        <div class="notification-header">${this.type}</div>
+        <div class="notification-body">
+          ${this.message}
+        </div>
+      </div>
+    </div>
+    `;
+
+    this.element = element.firstElementChild;
+    NotificationMessage.activePopup = this.element;
+  }
+
+  remove () {
+    this.element.remove();
+  }
+
+  destroy() {
+    this.remove();
+  }
 }
